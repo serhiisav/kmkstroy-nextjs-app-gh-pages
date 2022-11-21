22f15e6b7bce24dca153171aac61ee21ebdb0586
SerhiiVoshta <dellnames@gmail.com>
2022-11-21 16:09:58 +0200

    update api

--- pages/api/getGallery.js
+++ pages/api/getGallery.js
@@ -3,5 +3,6 @@ export const getGallery = async () => {
     const url = process.env.NODE_ENV === "development" ? process.env.DEV_URL : process.env.PROD_URL;
     const response = await fetch(url);
     const data = await response.json();
-    return data;
+    // console.log(data);
+    return { gallery: data };
 }
