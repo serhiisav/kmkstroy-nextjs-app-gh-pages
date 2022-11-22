ece28906771c1dc79a862f107c4a5edc59a1a09a
SerhiiVoshta <dellnames@gmail.com>
2022-11-22 21:45:11 +0200

    url #4

--- pages/api/getGallery.js
+++ pages/api/getGallery.js
@@ -1,7 +1,7 @@
 
 export const getGallery = async () => {
     // const url = process.env.NODE_ENV === "development" ? process.env.DEV_URL : process.env.PROD_URL;
-    const response = await fetch("https://github.com/serhiisav/kmkstroy-nextjs-app-gh-pages/blob/main/gallery.json");
+    const response = await fetch("https://github.com/serhiisav/kmkstroy-nextjs-app-gh-pages/blob/main/pages/api/data/gallery.js");
     const data = await response.json();
-    return { gallery: data };
+    return data;
 }
