2f27d8ab5efc75eb5791fe69a2ba6bc0581996cf
SerhiiVoshta <dellnames@gmail.com>
2022-11-22 09:19:32 +0200

    update url gallery data

--- pages/api/getGallery.js
+++ pages/api/getGallery.js
@@ -1,7 +1,7 @@
 
 export const getGallery = async () => {
     // const url = process.env.NODE_ENV === "development" ? process.env.DEV_URL : process.env.PROD_URL;
-    const response = await fetch("./api/gallery");
+    const response = await fetch("./kmkstroy-nextjs-app-gh-pages/api/gallery");
     const data = await response.json();
     return data;
 }
